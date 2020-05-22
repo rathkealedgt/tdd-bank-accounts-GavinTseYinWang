4fd20adf36da3c6f70e988c74324c104b2b7b14b
GavinTseYinWang <171096@trinityschools.nz>
2020-05-22 14:07:56 +1200

    May u help me sir

--- BankAccountTest/UnitTest1.vb
+++ BankAccountTest/UnitTest1.vb
@@ -3,7 +3,115 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestContructors()
+
+        ' Arrange
+        Dim AccountHolder As String = "Ms I. N. Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = 4.3
+        Dim Balance As Double = 10343.82
+        Dim CountryOfOrigin As String = "Isle of Man"
+
+        ' Act 
+        Dim Account1 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, InterestRate, Balance, CountryOfOrigin)
+        Dim Account2 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, InterestRate, Balance)
+        Dim Account3 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, InterestRate)
+
+        ' Assert
+        Assert.IsNotNull(Account1)
+        Assert.IsNotNull(Account2)
+        Assert.IsNotNull(Account3)
     End Sub
 
+    <TestMethod()> Public Sub TestGetAccountHolder()
+
+        ' Arrange
+        Dim AccountHolder As String = "Ms I. N. Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = 4.3
+        Dim Balance As Double = 10343.82
+        Dim CountryOfOrigin As String = "Isle of Man"
+        Dim Account1 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, InterestRate, Balance, CountryOfOrigin)
+
+        ' Act
+        Dim Name As String = Account1.GetAccountHolder()
+
+        ' Assert
+        Assert.AreEqual(Name, "Ms I. N. Cognito")
+
+    End Sub
+
+    <TestMethod()> Public Sub TestGetInterestRate()
+
+        ' Arrange
+        Dim AccountHolder As String = "Ms I. N. Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = 4.3
+        Dim Balance As Double = 10343.82
+        Dim CountryOfOrigin As String = "Isle of Man"
+        Dim Account1 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, Balance, InterestRate, CountryOfOrigin)
+
+        ' Act
+        Dim IR = Account1.GetInterestRate()
+
+        ' Assert
+        Assert.AreEqual(4.3, IR)
+
+    End Sub
+
+    <TestMethod()> Public Sub TestGetCountryOfOrigin()
+
+        ' Arrange
+        Dim AccountHolder As String = "Ms I. N. Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = 4.3
+        Dim Balance As Double = 10343.82
+        Dim CountryOfOrigin As String = "Isle of Man"
+        Dim Account1 As New BankAccounts.BankAccount(AccountHolder, AccountNumber, InterestRate, Balance, CountryOfOrigin)
+
+        ' Act
+        Dim Country = Account1.GetCountryOfOrigin()
+
+        ' Assert
+        Assert.AreEqual(Country, "Isle of Man")
+
+    End Sub
+
+    <TestMethod()> Public Sub TestSetInterestRate()
+        'Arrange 
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount()
+
+        ' Act 
+        Account1.SetInterestRate(5.1)
+
+        'Assert
+        Assert.AreEqual(5.1, Account1.GetInterestRate())
+
+
+    End Sub
+
+    <TestMethod()> Public Sub TestApplyInterestRate()
+        'p x r x t = i=> 37.07+10343.82 = 10380.89
+
+        'Arrange 
+        Dim Account1 As BankAccounts.BankAccount = Me.NewAccount()
+
+        ' Act 
+        Account1.ApplyInterest()
+
+        'Assert
+        Assert.AreEqual(Account1.GetBalance(), 10380.89)
+
+
+    End Sub
+
+    Private Function NewAccount() As BankAccounts.BankAccount
+        Dim AccountHolder As String = "Ms I. N. Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = 4.3
+        Dim Balance As Double = 10343.82
+        Dim CountryOfOrigin As String = "Isle of Man"
+        Return New BankAccounts.BankAccount(AccountHolder, AccountNumber, InterestRate, Balance, CountryOfOrigin)
+    End Function
 End Class
+
---
+++ BankAccounts/BankAccount.vb
@@ -0,0 +1,66 @@
+﻿'''<summary>
+'''    Represents a single bank account. 
+''' </summary>
+''' <author>Mr M</author>
+''' <date>06/05/20</date>
+Public Class BankAccount
+
+    Private AccountHolder As String
+    Private AccountNumber As String
+    Private InterestRate As Double
+    Private Balance As Double
+    Private CountryOfOrigin As String
+
+    Public Sub New(AccountHolder As String, AccountNumber As String, InterestRate As Double, Balance As Double, CountryOfOrigin As String)
+        Me.AccountHolder = AccountHolder
+        Me.AccountNumber = AccountNumber
+        Me.InterestRate = InterestRate
+        Me.Balance = Balance
+        Me.CountryOfOrigin = CountryOfOrigin
+
+    End Sub
+
+    Public Sub New(AccountHolder As String, AccountNumber As String, InterestRate As Double, Balance As Double)
+        Me.AccountHolder = AccountHolder
+        Me.AccountNumber = AccountNumber
+        Me.InterestRate = InterestRate
+        Me.Balance = Balance
+
+    End Sub
+
+    Public Sub New(AccountHolder As String, AccountNumber As String, InterestRate As Double)
+        Me.AccountHolder = AccountHolder
+        Me.AccountNumber = AccountNumber
+        Me.InterestRate = InterestRate
+
+    End Sub
+
+    Public Function GetAccountHolder() As String
+        Return Me.AccountHolder
+
+    End Function
+
+    Public Function GetInterestRate() As Double
+        Return Me.InterestRate
+
+    End Function
+
+    Public Function GetCountryOfOrigin() As String
+        Return Me.CountryOfOrigin
+
+    End Function
+
+    Public Function SetInterestRate(Rate As Double)
+        Me.InterestRate = Rate
+
+        Return Nothing
+    End Function
+
+    Public Function ApplyInterest()
+        Dim NewBalance As Double = Me.Balance + (Me.Balance * 0.043 * (1 / 12))
+
+        Me.Balance = Math.Round(NewBalance, 2)
+
+        Return Nothing
+    End Function
+End Class
